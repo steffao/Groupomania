92b3974cff2567b4888307995e2a2aa0587facd1
steffao <steffie.jeanjoseph@gmail.com>
2022-04-11 12:56:46 +1200

    create database and migration

---
+++ backend/migrations/20220407125936-create-comment.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      message_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Messages', // Message mis au pluriel
+          key: 'id'
+        }
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      attachment: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Comments');
+  }
+};
