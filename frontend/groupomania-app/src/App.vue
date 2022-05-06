05082e187766241fc4a7dcbc2dbc998703d78cd3
steffao <steffie.jeanjoseph@gmail.com>
2022-05-07 00:17:10 +1200

    header and navbar style

--- frontend/groupomania-app/src/App.vue
+++ frontend/groupomania-app/src/App.vue
@@ -1,12 +1,30 @@
 <template>
+  
   <div id="app">
+    <!-- Header -->
     <NavBar />
-    
+    <!-- Page Container -->
+    <main class="w3-container w3-content" style="max-width:1400px;margin-top:80px">
+      <div class="w3-row">
+        <router-view />
+      </div>      
+    </main>   
   </div>
   
-  <router-view />
+  
+  
 </template>
 
+<style lang="css">
+  @import '../src/assets/style/style.css';
+  @import url("https://www.w3schools.com/lib/w3-theme-blue-grey.css");
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans');
+  @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
+  
+  
+
+
+</style>
 <script>
 import NavBar from './components/NavBar.vue'
 import {getCookie} from './utils/cookie.js'
--- frontend/groupomania-app/src/components/NavBar.vue
+++ frontend/groupomania-app/src/components/NavBar.vue
@@ -1,19 +1,46 @@
 
 <template>
+
+    <!-- Navbar Logged In-->
     <nav v-if="token">
-        <div>Bonjour {{user.firstName}}  !</div>
-        <button @click="logoutUser">Logout</button>
+        <div class="w3-top">
+            <div class="w3-bar w3-theme-d2 w3-left-align w3-large">                
+                <a href="#" class="w3-bar-item w3-button w3-padding-large w3-theme-d4">
+                    <img class="w3-margin-right" src="../assets//images/groupomania_logo/icon-left-font-monochrome-white.svg" alt="Logo de Groupomania" style="max-height: 20px">
+                </a>
+                <div class="w3-dropdown-hover w3-hide-small w3-right">
+                    <button class="w3-button w3-padding-large" title="User Settings"><i class="fa fa-user"></i></button>     
+                    <div class="w3-dropdown-content w3-card-4 w3-bar-block" style="width:180px">
+                        <button @click="logoutUser" class="w3-bar-item w3-button">Déconnexion</button>                
+                        <button @click="logoutUser" class="w3-bar-item w3-button">Supprimer votre compte utilisateur</button>                
+                    </div>
+                </div>            
+            </div>
+        </div>
     </nav>
-    <nav v-else>
-        <router-link to="/login">Login</router-link>
-        <router-link to="/signup">Signup</router-link>
+
+    <!-- Navbar Logged Out-->
+    <nav v-else>        
+        <div class="w3-top">
+            <div class="w3-bar w3-theme-d2 w3-left-align w3-large">
+                <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-theme-d2" @click="toggleMenu"><i class="fa fa-bars"></i></a>
+                <a href="#" class="w3-bar-item w3-button w3-padding-large w3-theme-d4">
+                    <img class="w3-margin-right" src="../assets//images/groupomania_logo/icon-left-font-monochrome-white.svg" alt="Logo de Groupomania" style="max-height: 20px">
+                </a>
+                <router-link to="/login" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Connexion</router-link>
+                <router-link to="/signup" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Inscription</router-link>                
+            </div>
+        </div>
+        <!-- Navbar on small screens -->
+        <div id="submenu" :class="{ 'w3-show': isToggled }" class="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium w3-large w3-below-submenu">
+            <router-link to="/login" class="w3-bar-item w3-button w3-padding-large">Connexion</router-link>
+            <router-link to="/signup" class="w3-bar-item w3-button w3-padding-large">Inscription</router-link>
+        </div>        
     </nav>
+    
 </template>
 
 <style>
-a {
-    text-decoration: none;
-}
 </style>
 
 <script>
@@ -27,6 +54,7 @@ export default {
             email:'',
             password:'',
             errors: [],
+            isToggled : false,
         }
     },
     computed : {
@@ -41,6 +69,15 @@ export default {
             this.$store.dispatch('resetState');
             this.$router.push('/login')            
             
+        },
+        toggleMenu : function() {
+            if (this.isToggled){
+                    this.isToggled = false
+                } else {
+                    this.isToggled = true;
+                    
+                }
+
         }
     }
 }   
